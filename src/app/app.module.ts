9c4117b4765a5a3f835e9ee61ead24da00f14a88
Cameron Morefield <cmorefield@csumb.edu>
2019-04-28 10:00:08 +0800

    added fixes and test array

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PetitionFormComponent } from './petition-form/petition-form.component';
 import { PetitionComponent } from './petition/petition.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +14,8 @@ import { PetitionComponent } from './petition/petition.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/petition/petition.component.ts
+++ src/app/petition/petition.component.ts
@@ -10,7 +10,12 @@ import { GetsetpetService } from '../getsetpet.service';
 
 export class PetitionComponent implements OnInit {
   petitions: Pet[];
-  
+  testpet : Pet[] = [
+    { _id: 'a000', title: 'Petition1', descriptions: 'description1', numSignatures: 77},
+    { _id: 'a001', title: 'Petition2', descriptions: 'description2', numSignatures: 1},
+    { _id: 'a002', title: 'Petition3', descriptions: 'description3', numSignatures: 3},
+    { _id: 'a003', title: 'Petition4', descriptions: 'description4', numSignatures: 770000000000}
+  ];
   
   constructor(private getsetpetService: GetsetpetService) { }
 
