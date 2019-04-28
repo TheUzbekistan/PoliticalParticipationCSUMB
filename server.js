3ac37eba1d8bd1d0e7cddcb330ac298605e7396d
theuzbekistan <dbeach@csumb.edu>
2019-04-27 19:10:24 -0700

    herokue server filfe

---
+++ server.js
@@ -0,0 +1,31 @@
+//Install express server
+
+const express = require('express');
+
+const path = require('path');
+
+
+
+const app = express();
+
+
+
+// Serve only the static files form the dist directory
+
+app.use(express.static(__dirname + '/dist/PoliticalParticipationCSUMB'));
+
+
+
+app.get('/*', function(req,res) {
+
+    
+
+res.sendFile(path.join(__dirname+'/dist/PoliticalParticipationCSUMB/index.html'));
+
+});
+jslkfaj
+
+
+// Start the app by listening on the default Heroku port
+
+app.listen(process.env.PORT || 8080); 
