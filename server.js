bba4ca2e0938a32604b1de0021f565136b36d7d7
Erik Ring-Walters <erik.ring.walters@gmail.com>
2019-05-04 14:42:19 -0700

    deployment

--- server.js
+++ server.js
@@ -1,32 +1,16 @@
 //Install express server
-
 const express = require('express');
-
 const path = require('path');
-
-
 
 const app = express();
 
-
-
 // Serve only the static files form the dist directory
-
 app.use(express.static(__dirname + '/dist/PoliticalParticipationCSUMB'));
 
+app.get('/*', function(req,res) {
 
-// app.get('/*', function(req,res) {
-
-    
-
-// res.sendFile(path.join(__dirname+'/dist/PoliticalParticipationCSUMB/index.html'));
-
-// });
-app.get('*', function (req, res) {
-    const index = path.join(__dirname, 'build', 'index.html');
-    res.sendFile(index);
-  });
+res.sendFile(path.join(__dirname+'/dist/PoliticalParticipationCSUMB/index.html'));
+});
 
 // Start the app by listening on the default Heroku port
-
-app.listen(process.env.PORT || 8080); 
+app.listen(process.env.PORT || 8080);
