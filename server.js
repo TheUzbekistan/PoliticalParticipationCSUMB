333e472eb5329937a221ae501b38c86837012406
Erik Ring-Walters <erik.ring.walters@gmail.com>
2019-05-04 14:55:07 -0700

    lowercase p

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/PoliticalParticipationCSUMB'));
+app.use(express.static(__dirname + '/dist/politicalParticipationCSUMB'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/PoliticalParticipationCSUMB/index.html'));
+res.sendFile(path.join(__dirname+'/dist/politicalParticipationCSUMB/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
