01a921a893703f5749d97fe6f856e3a6301e1c89
DerekMcFate <dmcfate@csumb.edu>
2019-05-04 16:25:33 -0700

    s less

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,10 +11,10 @@ export class AppComponent {
   petitions: Pet[];
   supportPet: Pet;
   testpet : Pet[] = [
-    { _id: 'a000', title: 'Petition1', descriptions: 'Information about this petition the jclkds l;kcj as;lkjdcljasdlck jal;ks dclk;jasdlkcjlkjsadfnlcjla sjef lknalksdj flk vnsdkljflwe;nicjasdkjfnlaksjdfl;javk elkjskldfjsdk;lckafjl ksdjk vkajsfjlksdvn ljasefn klsjvlkaewjkflkjsadkvnlaksdjvlkenfkljsdlkvnklajsd lkfn;ihvljkjasjlkfjasdlkjkdsfjsdds fkds f sdfl sdfjksdfkl avklaskjv kldsjfklj avlkjslkaj lkv dsklvnsdkcn kjsdfjnv klsdjlkfj kv k;ljsoiehfisudvhafskjhsdkl j lvknvi;sglahsvnavsk ghihasdfh', numSignatures: 77},
-    { _id: 'a001', title: 'Petition2', descriptions: 'description2', numSignatures: 1},
-    { _id: 'a002', title: 'Petition3', descriptions: 'description3', numSignatures: 3},
-    { _id: 'a003', title: 'Petition4', descriptions: 'description4', numSignatures: 770000000000}
+    { _id: 'a000', title: 'Petition1', description: 'Information about this petition the jclkds l;kcj as;lkjdcljasdlck jal;ks dclk;jasdlkcjlkjsadfnlcjla sjef lknalksdj flk vnsdkljflwe;nicjasdkjfnlaksjdfl;javk elkjskldfjsdk;lckafjl ksdjk vkajsfjlksdvn ljasefn klsjvlkaewjkflkjsadkvnlaksdjvlkenfkljsdlkvnklajsd lkfn;ihvljkjasjlkfjasdlkjkdsfjsdds fkds f sdfl sdfjksdfkl avklaskjv kldsjfklj avlkjslkaj lkv dsklvnsdkcn kjsdfjnv klsdjlkfj kv k;ljsoiehfisudvhafskjhsdkl j lvknvi;sglahsvnavsk ghihasdfh', numSignatures: 77},
+    { _id: 'a001', title: 'Petition2', description: 'description2', numSignatures: 1},
+    { _id: 'a002', title: 'Petition3', description: 'description3', numSignatures: 3},
+    { _id: 'a003', title: 'Petition4', description: 'description4', numSignatures: 770000000000}
   ];
   title = 'The Otter Petitions';
 
--- src/app/petition/petition.component.ts
+++ src/app/petition/petition.component.ts
@@ -11,10 +11,10 @@ import { GetsetpetService } from '../getsetpet.service';
 export class PetitionComponent implements OnInit {
   petitions: Pet[];
   testpet : Pet[] = [
-    { _id: 'a000', title: 'Petition1', descriptions: 'description1', numSignatures: 77},
-    { _id: 'a001', title: 'Petition2', descriptions: 'description2', numSignatures: 1},
-    { _id: 'a002', title: 'Petition3', descriptions: 'description3', numSignatures: 3},
-    { _id: 'a003', title: 'Petition4', descriptions: 'description4', numSignatures: 770000000000}
+    { _id: 'a000', title: 'Petition1', description: 'description1', numSignatures: 77},
+    { _id: 'a001', title: 'Petition2', description: 'description2', numSignatures: 1},
+    { _id: 'a002', title: 'Petition3', description: 'description3', numSignatures: 3},
+    { _id: 'a003', title: 'Petition4', description: 'description4', numSignatures: 770000000000}
   ];
   
   constructor(private getsetpetService: GetsetpetService) { }
